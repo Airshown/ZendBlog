5eaaa354c159346a809239fd700c5314a15d9d24
Ludovic COURTIAL <mr.erdah@gmail.com>
2016-02-21 12:42:11 +0100

    [ADD] Ajout du dump de la base + ajout du début du module commentaire

--- module/Blog/config/module.config.routes.php
+++ module/Blog/config/module.config.routes.php
@@ -89,6 +89,65 @@ return array(
                         ),
                     ),
                 ),
+
+                'comments' => array(
+                    'type'    => 'Zend\Mvc\Router\Http\Literal',
+                    'options' => array(
+                        'route'    => '/comments',
+                        'defaults' => array(
+                            'controller'    => 'Blog\Controller\Comment',
+                            'action'        => 'indexAdmin',
+                        ),
+                    ),
+                    'may_terminate' => true,
+                    'child_routes' => array(
+                        'list' => array(
+                            'type'    => 'Zend\Mvc\Router\Http\Segment',
+                            'options' => array(
+                                'route'    => '[/page/:page]',
+                                'defaults' => array(
+                                    'page' => 1,
+                                ),
+                                'constraints' => array(
+                                    'page' => '[0-9]+',
+                                ),
+                            ),
+                        ),
+                        'delete' => array(
+                            'type'    => 'Zend\Mvc\Router\Http\Segment',
+                            'options' => array(
+                                'route'    => '/delete/:id',
+                                'defaults' => array(
+                                    'action' => 'delete',
+                                ),
+                                'constraints' => array(
+                                    'id' => '[0-9]+',
+                                ),
+                            ),
+                        ),
+                        'edit' => array(
+                            'type'    => 'Zend\Mvc\Router\Http\Segment',
+                            'options' => array(
+                                'route'    => '/edit/:id',
+                                'defaults' => array(
+                                    'action' => 'edit',
+                                ),
+                                'constraints' => array(
+                                    'id' => '[0-9]+',
+                                ),
+                            ),
+                        ),
+                        'add' => array(
+                            'type'    => 'Zend\Mvc\Router\Http\Literal',
+                            'options' => array(
+                                'route'    => '/add',
+                                'defaults' => array(
+                                    'action' => 'add',
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
             ),
         ),
         'posts' => array(
@@ -97,57 +156,6 @@ return array(
                 'route'    => '/posts',
                 'defaults' => array(
                     'controller'    => 'Blog\Controller\Post',
-                    'action'        => 'index',
-                ),
-            ),
-            'may_terminate' => true,
-            'child_routes' => array(
-                'list' => array(
-                    'type'    => 'Zend\Mvc\Router\Http\Segment',
-                    'options' => array(
-                        'route'    => '[/page/:page][/category/:category][/author/:author]',
-                        'defaults' => array(
-                            'page' => 1,
-                        ),
-                        'constraints' => array(
-                            'page'     => '[0-9]+',
-                            'category' => '[a-zA-Z\-0-9]+',
-                            'author'   => '[0-9]+',
-                        ),
-                    ),
-                ),
-                'show' => array(
-                    'type'    => 'Zend\Mvc\Router\Http\Segment',
-                    'options' => array(
-                        'route'    => '/:slug',
-                        'defaults' => array(
-                            'action' => 'show',
-                        ),
-                        'constraints' => array(
-                            'slug' => '[a-zA-Z\-0-9]+',
-                        ),
-                    ),
-                ),
-                'category' => array(
-                    'type'    => 'segment',
-                    'options' => array(
-                        'route'    => 'category/:slug',
-                        'defaults' => array(
-                            'action' => 'category',
-                        ),
-                        'constraints' => array(
-                            'slug' => '[a-zA-Z\-0-9]+',
-                        ),
-                    ),
-                ),
-            ),
-        ),
-        'comments' => array(
-            'type'    => 'Zend\Mvc\Router\Http\Literal',
-            'options' => array(
-                'route'    => '/comments',
-                'defaults' => array(
-                    'controller'    => 'Blog\Controller\Comment',
                     'action'        => 'index',
                 ),
             ),
